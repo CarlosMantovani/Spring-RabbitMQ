e5da6f3c8d8577121e24bdd05e0eeb19639afb6e
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-23 11:32:32 -0300

    Feature: Implementando endPoint para listar cartões por renda

--- mscartoes/src/main/java/mantovanidev/mscartoes/application/CartoesController.java
+++ mscartoes/src/main/java/mantovanidev/mscartoes/application/CartoesController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cartoes")
 @RequiredArgsConstructor
@@ -23,5 +25,9 @@ public class CartoesController {
         service.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
+       List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
 }
