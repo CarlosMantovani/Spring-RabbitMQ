4cf2eac4b0aa33772b6a0d59a4972a5ed1f8c1d2
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-23 17:53:02 -0300

    Feature: Implementando metodo de buscar cartao do cliente utilizando o cpf como busca e o open feing para fazer a  comunicação entre os ms

--- msavaliadorcredito/src/main/java/mantovanidev/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/mantovanidev/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,19 +1,29 @@
 package mantovanidev.msavaliadorcredito.application;
 
 import lombok.RequiredArgsConstructor;
+import mantovanidev.msavaliadorcredito.domain.model.CartaoCliente;
 import mantovanidev.msavaliadorcredito.domain.model.DadosCliente;
 import mantovanidev.msavaliadorcredito.domain.model.SituacaoCliente;
+import mantovanidev.msavaliadorcredito.infra.clients.CartaoResourceClient;
 import mantovanidev.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartaoResourceClient cartaoResourceClient;
     public SituacaoCliente obterSituacaoCliente(String cpf){
        ResponseEntity<DadosCliente> dadosClienteResponse =  clienteResourceClient.dadosCliente(cpf);
-       return SituacaoCliente.builder().cliente(dadosClienteResponse.getBody()).build();
+       ResponseEntity<List<CartaoCliente>> cartaoResponse=cartaoResourceClient.getCartoesBycCliente(cpf);
+       return SituacaoCliente.builder()
+               .cliente(dadosClienteResponse.getBody())
+               .cartoes(cartaoResponse.getBody())
+               .build();
     }
+
 }
