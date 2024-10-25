3970f6c0079ca7060b262d55a215e4bf551865df
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-25 10:16:28 -0300

    Refactor: Adicionando @bean e alterando nome da fila

--- msavaliadorcredito/src/main/java/mantovanidev/msavaliadorcredito/config/MQConfig.java
+++ msavaliadorcredito/src/main/java/mantovanidev/msavaliadorcredito/config/MQConfig.java
@@ -2,13 +2,16 @@ package mantovanidev.msavaliadorcredito.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfig {
-    @Value("${mq.queues.emissao-cartoes}")
+    @Value("${mq.queue.emissao-cartoes}")
     private String emissaoCartoesFila;
+
+    @Bean
     public Queue queueEmissaoCartoes(){
-        return new Queue(emissaoCartoesFila,true);
+        return new Queue( emissaoCartoesFila , true);
     }
 }
