81fbbece1f78a1770ebe197ac27af02682afe728
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-25 15:39:40 -0300

    Feature: Implementando spring security no discovery server

--- eurekaserver/src/main/java/mantovanidev/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/mantovanidev/eurekaserver/EurekaserverApplication.java
@@ -1,5 +1,6 @@
 package mantovanidev.eurekaserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,6 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("EUREKA_USER", dotenv.get("EUREKA_USER"));
+		System.setProperty("EUREKA_PASSWORD", dotenv.get("EUREKA_PASSWORD"));
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
