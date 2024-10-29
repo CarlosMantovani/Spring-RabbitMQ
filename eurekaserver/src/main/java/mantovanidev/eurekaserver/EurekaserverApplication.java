cf70d0385571c61e21dab54a1bcf696e2a755823
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-29 15:52:40 -0300

    refactor: .env delete

--- eurekaserver/src/main/java/mantovanidev/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/mantovanidev/eurekaserver/EurekaserverApplication.java
@@ -1,6 +1,6 @@
 package mantovanidev.eurekaserver;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
